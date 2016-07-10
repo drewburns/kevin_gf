a481ff143c290fa37291b22853d485453f923441
drewburns <drewburnsbab@gmail.com>
2016-07-10 15:25:04 -0400

    done

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
 $(document).foundation()
+
+$.fn.extend({
+   qcss: function(css) {
+      return $(this).queue(function(next) {
+         $(this).css(css);
+         next();
+      });
+   }
+});
+
+
+$( "#enter" ).click(function() {
+  var input = $('#input').val();
+  if (input == "love") {
+  	$("form").hide();
+  	$("#content").removeClass('content1').addClass('content2');
+  	
+  } else {
+  	$("#input").qcss({ color: 'red' }).delay(200).qcss({color: 'lime'})  }
+});
